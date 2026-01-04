26aed0290cdfcfc6325b7059b7c77fad7413f2c2
justin <jz7233@rit.edu>
2026-01-03 21:48:56 -0500

    added top nav

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -6,18 +6,22 @@ export default function Home() {
     <div>
       <main>
         <div className= "relative w-screen h-screen overflow-hidden">
+          {/* Top Nav */}
+          <nav className="absolute top-0 left-0 w-full p-4 flex justify-between items-center z-10">
+            <div className="text-white font-bold text-xl">JUSTIN ZHAO</div>
+          </nav>
           {/* Aurora Background */}
           <div className="absolute inset-0 w-full h-full -z-10">
             <Aurora
               colorStops={["#3A29FF", "#FF94B4", "#FF3232"]}
               blend={0.5}
               amplitude={1.0}
-              speed={0.5}
+              speed={1}
             />
           </div>
           {/* Centered Tet */}
           <div className="flex w-full h-full items-center justify-center">
-            <h2 className="m-auto text-4xl text-center w-full ">Hi, I'm Justin Zhao</h2>
+            <h2 className="m-auto font-bold text-4xl text-center w-full ">I CODE, DESIGN, AND BUILD</h2>
           </div>
         </div>
       </main>
